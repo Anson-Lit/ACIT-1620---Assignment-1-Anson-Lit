61e5de7001cd684e7bb95ba0f9a73ae3d37069d3
Anson <ansonlit@live.ca>
2020-11-29 16:23:12 -0800

    Add Hide notetaking area functionality

--- JS/my_js.js
+++ JS/my_js.js
@@ -1,10 +1,14 @@
-let theme_btn = document.querySelector(".button_2")
+let newNoteBtn = document.querySelector(".button_1")
+let themeBtn = document.querySelector(".button_2")
+let saveBtn = document.querySelector(".button_3")
+let cancelBtn = document.querySelector(".button_4")
+let button_class = document.querySelector(".Placeholder")
 
 function changeThemes() {
     let notes_class = document.querySelector(".notes");
-    let button_class = document.querySelector(".Placeholder")
-    if (theme_btn.innerHTML === "Dark Theme") {
-        theme_btn.innerHTML = "Light Theme";
+
+    if (themeBtn.innerHTML === "Dark Theme") {
+        themeBtn.innerHTML = "Light Theme";
         notes_class.style.backgroundColor = "#740023"
         button_class.style.backgroundColor = "#740023"
     } else {
@@ -14,5 +18,14 @@ function changeThemes() {
     }
 }
 
-theme_btn.addEventListener("click", changeThemes)
+themeBtn.addEventListener("click", changeThemes)
     //changes theme
+    //class list toggle?
+
+function hide() {
+    let textBox = document.querySelector(".textbox")
+    cancelBtn.style.visibility = "hidden"
+    saveBtn.style.visibility = "hidden"
+    textBox.style.visibility = "hidden"
+}
+cancelBtn.addEventListener("click", hide)
