257c4a10929c43da6f0debc634156450231812d9
Anson <ansonlit@live.ca>
2020-11-29 16:57:25 -0800

    Add functionality to clear text in textbox and to make textbox reappeart

--- JS/my_js.js
+++ JS/my_js.js
@@ -3,6 +3,7 @@ let themeBtn = document.querySelector(".button_2")
 let saveBtn = document.querySelector(".button_3")
 let cancelBtn = document.querySelector(".button_4")
 let button_class = document.querySelector(".Placeholder")
+let textBox = document.querySelector(".textbox-child")
 
 function changeThemes() {
     let notes_class = document.querySelector(".notes");
@@ -12,7 +13,7 @@ function changeThemes() {
         notes_class.style.backgroundColor = "#740023"
         button_class.style.backgroundColor = "#740023"
     } else {
-        theme_btn.innerHTML = "Dark Theme";
+        themeBtn.innerHTML = "Dark Theme";
         notes_class.style.backgroundColor = "#BD8B9C"
         button_class.style.backgroundColor = "#4B8F8C"
     }
@@ -23,9 +24,21 @@ themeBtn.addEventListener("click", changeThemes)
     //class list toggle?
 
 function hide() {
-    let textBox = document.querySelector(".textbox")
-    cancelBtn.style.visibility = "hidden"
-    saveBtn.style.visibility = "hidden"
-    textBox.style.visibility = "hidden"
+    cancelBtn.classList.toggle("hidden-style")
+    saveBtn.classList.toggle("hidden-style")
+    textBox.classList.toggle("hidden-style")
 }
 cancelBtn.addEventListener("click", hide)
+
+function showAndClear() {
+    if (cancelBtn.classList.contains("hidden-style")) {
+        cancelBtn.classList.toggle("hidden-style")
+        saveBtn.classList.toggle("hidden-style")
+        textBox.classList.toggle("hidden-style")
+    } else {
+        textBox.value = ""
+    }
+
+}
+
+newNoteBtn.addEventListener("click", showAndClear)
