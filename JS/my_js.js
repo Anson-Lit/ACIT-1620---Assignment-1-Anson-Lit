fa1ed12187e1cbc271c8cb59ca695d5db70e9bba
Anson <ansonlit@live.ca>
2020-12-01 09:48:25 -0800

    update changeThemes function to include buttons changing colours when theme is changed

--- JS/my_js.js
+++ JS/my_js.js
@@ -7,15 +7,21 @@ let textBox = document.querySelector(".textbox-child")
 
 function changeThemes() {
     let notes_class = document.querySelector(".notes");
-
+    btnArray = [newNoteBtn, themeBtn, saveBtn, cancelBtn]
     if (themeBtn.innerHTML === "Dark Theme") {
         themeBtn.innerHTML = "Light Theme";
         notes_class.style.backgroundColor = "#740023"
         button_class.style.backgroundColor = "#740023"
+        notes_class.style.color = "white"
+        for (items of btnArray)
+            items.classList.toggle("darkColor")
     } else {
         themeBtn.innerHTML = "Dark Theme";
         notes_class.style.backgroundColor = "#BD8B9C"
         button_class.style.backgroundColor = "#4B8F8C"
+        notes_class.style.color = "black"
+        for (items of btnArray)
+            items.classList.toggle("darkColor")
     }
 }
 
